201cf210d7d56c50ce2b83f3e59f9da628b546f1
gaojie <1226898330@qq.com>
2017-08-19 14:46:31 +0800

    up 提交失败

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,26 +12,61 @@ declare var $: any;
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    //动态样式
-    @HostBinding('class.layout-fixed') get isFixed() { return this.settings.layout.isFixed; };
-    @HostBinding('class.aside-collapsed') get isCollapsed() { return this.settings.layout.isCollapsed; };
-    @HostBinding('class.layout-boxed') get isBoxed() { return this.settings.layout.isBoxed; };
-    @HostBinding('class.layout-fs') get useFullLayout() { return this.settings.layout.useFullLayout; };
-    @HostBinding('class.hidden-footer') get hiddenFooter() { return this.settings.layout.hiddenFooter; };
-    @HostBinding('class.layout-h') get horizontal() { return this.settings.layout.horizontal; };
-    @HostBinding('class.aside-float') get isFloat() { return this.settings.layout.isFloat; };
-    @HostBinding('class.offsidebar-open') get offsidebarOpen() { return this.settings.layout.offsidebarOpen; };
-    @HostBinding('class.operationpage-open') get operationpageOpen() { return this.settings.layout.operationpageOpen; }; // 是否显示右侧操作页面 by 立坤
-    @HostBinding('class.aside-toggled') get asideToggled() { return this.settings.layout.asideToggled; };
-    @HostBinding('class.aside-collapsed-text') get isCollapsedText() { return this.settings.layout.isCollapsedText; };
+  //动态样式
+  @HostBinding('class.layout-fixed') get isFixed() {
+    return this.settings.layout.isFixed;
+  };
 
-    constructor(public settings: SettingsService, private cookieService:CookieService, private location:Location,private router:Router) { }
+  @HostBinding('class.aside-collapsed') get isCollapsed() {
+    return this.settings.layout.isCollapsed;
+  };
 
-    ngOnInit() {
-        $(document).on('click', '[href="#"]', e => e.preventDefault());
-        //登录状态检测
-        // this.checkLogin();
-    }
+  @HostBinding('class.layout-boxed') get isBoxed() {
+    return this.settings.layout.isBoxed;
+  };
+
+  @HostBinding('class.layout-fs') get useFullLayout() {
+    return this.settings.layout.useFullLayout;
+  };
+
+  @HostBinding('class.hidden-footer') get hiddenFooter() {
+    return this.settings.layout.hiddenFooter;
+  };
+
+  @HostBinding('class.layout-h') get horizontal() {
+    return this.settings.layout.horizontal;
+  };
+
+  @HostBinding('class.aside-float') get isFloat() {
+    return this.settings.layout.isFloat;
+  };
+
+  @HostBinding('class.offsidebar-open') get offsidebarOpen() {
+    return this.settings.layout.offsidebarOpen;
+  };
+
+  @HostBinding('class.operationpage-open') get operationpageOpen() {
+    return this.settings.layout.operationpageOpen;
+  }; // 是否显示右侧操作页面 by 立坤
+  @HostBinding('class.aside-toggled') get asideToggled() {
+    return this.settings.layout.asideToggled;
+  };
+
+  @HostBinding('class.aside-collapsed-text') get isCollapsedText() {
+    return this.settings.layout.isCollapsedText;
+  };
+
+  @ViewChild("altComponent", {read: ViewContainerRef}) container: ViewContainerRef;
+
+
+  constructor(public settings: SettingsService, private cookieService: CookieService, private location: Location, private router: Router) {
+  }
+
+  ngOnInit() {
+    $(document).on('click', '[href="#"]', e => e.preventDefault());
+    //登录状态检测
+    // this.checkLogin();
+  }
 
 
 
