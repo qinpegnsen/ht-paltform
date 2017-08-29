71a24cae2b979bbf2dcf2fee83e149398643e012
qps <744056574@qq.com>
2017-08-29 17:17:24 +0800

    up form 公共服务的删除样式 up 所有文件ajax请求文件时候的大小写 add 快递公司组件

---
+++ src/app/routes/operation/express/right-page/right-page.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import {SettingsService} from "../../../../core/settings/settings.service";
+import {ActivatedRoute} from "@angular/router";
+import {SubmitService} from "../../../../core/forms/submit.service";
+import {ExpressComponent} from "../express.component";
+import {AddArticleSortService} from "app/routes/operation/add-article-sort/add-article-sort.service";
+
+@Component({
+  selector: 'app-right-page',
+  templateUrl: './right-page.component.html',
+  styleUrls: ['./right-page.component.scss']
+})
+export class RightPageComponent implements OnInit {
+
+  public type:string; //储存地址栏传递过来的类型，从而显示不同的页面
+
+  public id:number; //储存地址栏传递过来的id，从而获取当前id的内容，并修改
+
+  public isUseList:any; //是否启用的数组
+
+  public editData:any; //根据id查询出来的数据
+
+  public bingID:string="Y"; //是否启用的数组
+
+
+  constructor(public settings: SettingsService,private routeInfo: ActivatedRoute,public service:SubmitService,public parent:ExpressComponent,public selectById: AddArticleSortService) {
+    this.settings.showRightPage("30%");
+  }
+
+  ngOnInit() {
+    this.type = this.routeInfo.snapshot.queryParams['type'];
+
+    /**
+     * 获取地址栏的参数，并且根据id查询当前id的信息，从而做修改
+     * @type {any}
+     * 如果id存在的话，就说明是修改，这时候才执行以下的代码
+     */
+    this.id = this.routeInfo.snapshot.queryParams['id'];
+    if(this.id){
+      let url='/basicExpress/loadBasicExpressById';
+      let data={
+        id:this.id
+      }
+      this.editData=this.selectById.queryClassById(url,data);
+    }
+
+    /**
+     * 是否启用的数组
+     * @type {[{id: string; name: string},{id: string; name: string}]}
+     */
+    this.isUseList=[
+      {id:'Y',name:'启用'},
+      {id:'N',name:'停用'}
+    ]
+  }
+
+  // 取消
+  cancel(){
+    this.settings.closeRightPageAndRouteBack(); //关闭右侧滑动页面
+  }
+
+  // 提交
+  submit(obj){
+    if(this.type=='add'){
+      let url='/basicExpress/addBasicExpress';
+      let data={
+        expressCode:obj.expressCode,
+        expressName:obj.expressName,
+        expressUrl:obj.expressUrl,
+        isUse:obj.isUse
+      }
+      this.service.postRequest(url,data);
+    }else if(this.type=="edit"){
+      let url='/basicExpress/updateBasicExpress';
+      let data={
+        id:this.id,
+        expressCode:obj.expressCode,
+        expressName:obj.expressName,
+        expressUrl:obj.expressUrl,
+        isUse:obj.isUse
+      }
+      this.service.putRequest(url,data);
+    }
+    this.settings.closeRightPageAndRouteBack(); //关闭右侧滑动页面
+    this.parent.queryExpressList()
+  }
+}
