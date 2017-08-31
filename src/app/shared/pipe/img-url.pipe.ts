0be1728ff8470e52e9fb29504ab48843f6bcab4c
gaojie <1226898330@qq.com>
2017-08-31 10:13:25 +0800

    up 图片本地预览管道注释

--- src/app/shared/pipe/img-url.pipe.ts
+++ src/app/shared/pipe/img-url.pipe.ts
@@ -12,8 +12,9 @@ export class ImgUrlPipe implements PipeTransform {
   transform(value: File, args?: any): any {
     let src, url = window.URL;
     if(url){
-      src = url.createObjectURL(value);
+      src = url.createObjectURL(value);// 创建本地路径
     }
+    // 解决angular中本地blob路径"unsafe"错误
     return this.sanitizer.bypassSecurityTrustUrl(src);
   }
 
