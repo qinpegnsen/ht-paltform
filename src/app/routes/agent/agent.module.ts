ba9b3db198d30d4c8b804a5a81952c707df27176
yanweiyi <m15901019392_1@163.com>
2017-09-01 19:53:52 +0800

    up 添加了公共扩展样式 up add-agent 修改添加代理商

--- src/app/routes/agent/agent.module.ts
+++ src/app/routes/agent/agent.module.ts
@@ -8,28 +8,30 @@ import {SharedModule} from "../../shared/shared.module";
 import { RightpageComponent } from './rightpage/rightpage.component';
 import { AddAgentComponent } from './add-agent/add-agent.component';
 import {AgentpersonService} from "./agentperson/agentperson.service";
+import {FileUploader} from "ng2-file-upload/index";
+import {FileUploadModule} from "ng2-file-upload/index";
 
 // 子路由，用于页面嵌套显示
 const appChildRoutesfile: Routes = [
-  {path: 'rightpage', component: RightpageComponent},
+  {path: 'rightpage', component: RightpageComponent}
 ];
 // 子路由，用于页面嵌套显示
 const appChildRoutes: Routes = [
   {path: 'rightpage', component: RightpageComponent},
-  {path: 'appAgent', component: AddAgentComponent,children: appChildRoutesfile},
+  {path: 'appAgent', component: AddAgentComponent,children: appChildRoutesfile}
 ];
 const routes: Routes = [
   {path: '', redirectTo:'agentperson'},
   {path: 'agentperson', component: AgentpersonComponent,children: appChildRoutes},
   {path: 'region', component: RegionComponent,children: appChildRoutes},
-  {path: 'stock', component:StockComponent},
-
+  {path: 'stock', component:StockComponent}
 ];
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    SharedModule
+    SharedModule,
+    FileUploadModule
   ],
   providers:[AgentpersonService],
   declarations: [AgentpersonComponent, RegionComponent, StockComponent, RightpageComponent, AddAgentComponent]
