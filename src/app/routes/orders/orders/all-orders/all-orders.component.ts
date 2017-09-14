7b3ea39ce44410abb40dd49291ccface7897d3a2
gaojie <1226898330@qq.com>
2017-09-14 21:39:23 +0800

    up 订单布局 扩展样式

--- src/app/routes/orders/orders/all-orders/all-orders.component.ts
+++ src/app/routes/orders/orders/all-orders/all-orders.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {OrdersComponent} from "../orders.component";
+import {Page} from "../../../../core/page/page";
+import {PageEvent} from "angular2-datatable";
+import {isUndefined} from "util";
+import {SubmitService} from "../../../../core/forms/submit.service";
 
 @Component({
   selector: 'app-all-orders',
@@ -8,11 +12,34 @@ import {OrdersComponent} from "../orders.component";
 })
 export class AllOrdersComponent implements OnInit {
 
-  constructor(private parentComp:OrdersComponent) { }
+  public goodsList: Page = new Page();
+  constructor(private parentComp:OrdersComponent,
+              private submit: SubmitService,) { }
 
   ngOnInit() {
     let me = this;
-    me.parentComp.orderType = 1
+    me.parentComp.orderType = 1;
+    me.queryDatas(1)
   }
 
+  /**
+   * 查询列表
+   * @param event
+   * @param curPage
+   */
+  public queryDatas(curPage, event?: PageEvent) {
+    let _this = this, activePage = 1;
+    if (typeof event !== 'undefined') {
+      activePage = event.activePage;
+    } else if (!isUndefined(curPage)) {
+      activePage = curPage;
+    }
+    let requestUrl = '/goodsQuery/query';
+    let requestData = {
+      curPage: activePage,
+      pageSize: 10,
+      sortColumns: '',
+    };
+    _this.goodsList = new Page(_this.submit.getData(requestUrl, requestData));
+  }
 }
