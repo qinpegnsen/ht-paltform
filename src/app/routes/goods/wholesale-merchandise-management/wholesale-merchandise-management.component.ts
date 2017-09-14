87e71d60e9819336a30c62d07b64391dd34c97e4
xiejiaojiao <2583206346@qq.com>
2017-09-14 15:52:57 +0800

    up  批发商品管理中的商品名称，品牌名，分类的搜索，是否允许批发，批发价的修改

--- src/app/routes/goods/wholesale-merchandise-management/wholesale-merchandise-management.component.ts
+++ src/app/routes/goods/wholesale-merchandise-management/wholesale-merchandise-management.component.ts
@@ -3,8 +3,9 @@ import {PageEvent} from "../../../shared/directives/ng2-datatable/DataTable";
 import {AjaxService} from "../../../core/services/ajax.service";
 import {SubmitService} from "../../../core/forms/submit.service";
 import {Page} from "../../../core/page/page";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {AppComponent} from "../../../app.component";
+import {isNullOrUndefined} from "util";
 @Component({
   selector: 'app-wholesale-merchandise-management',
   templateUrl: './wholesale-merchandise-management.component.html',
@@ -13,12 +14,31 @@ import {AppComponent} from "../../../app.component";
 export class WholesaleMerchandiseManagementComponent implements OnInit {
 
   private data: Page = new Page();
+  private goodsName: any = ''; //商品名
+  private brandName: any = ''; //品牌名
+  private kindId: any = ''; //品牌名
+  public _goods = [];
   constructor(private ajax: AjaxService,private submit: SubmitService,private router:Router) { }
 
   ngOnInit() {
     this.qeuryAllService();
   }
 
+  /**
+   * 品牌名称，品牌名搜索
+   */
+  search(){
+    this.qeuryAllService();
+  }
+
+  /**
+   * 选择分类
+   * @param data  选择分类组件输出数据
+   */
+  getKind(data) {
+    this.kindId = data.kindId;
+    this.qeuryAllService();
+  }
   /**
    * 批发商品管理--查询分页
    */
@@ -29,9 +49,9 @@ export class WholesaleMerchandiseManagementComponent implements OnInit {
     let data={
       curPage: activePage,
       pageSize:10,
-      kindId:'',
-      goodsName:'',
-      brandName:'',
+      kindId:me.kindId,
+      goodsName:me.goodsName,
+      brandName:me.brandName,
       state: '',
       goodsAudit: '',
       sortColumns:'',
@@ -45,21 +65,44 @@ export class WholesaleMerchandiseManagementComponent implements OnInit {
    * 是否允许批发
    */
   startState(data) {
-    if (data.state == "HIDE") data.state = "SHOW"; else data.state = "HIDE";
-    let url = "/goodsEdit/updateIsBatch", _this = this;
-    this.ajax.put({
-      url: url,
-      data: {
-        goodsCode:'' ,
-        isBatch:'',
-      },
-      success: (res) => {
-        if (res.success) AppComponent.rzhAlt("success",res.info);
-        else AppComponent.rzhAlt("error","操作失败");
-      },
-      error: (data) => {
-        AppComponent.rzhAlt("error","修改功能操作状态失败");
+    let _this = this, isBatch;
+    if(data.isBatch == 'Y') data.isBatch = 'N';
+    else data.isBatch = 'Y';
+    if(!isNullOrUndefined(data.goodsPrice.batchPrice)&& data.goodsPrice.batchPrice != 0 && data.goodsPrice.batchPrice != ''){
+      let url = "/goodsEdit/updateIsBatch";
+      this.ajax.put({
+        url: url,
+        data: {
+          goodsCode:data.goodsCode ,
+          isBatch:data.isBatch,
+        },
+        success: (res) => {
+          console.log("█ res ►►►",  res);
+          if (res.success) AppComponent.rzhAlt("success",res.info);
+          else AppComponent.rzhAlt("error",res.info);
+          _this.qeuryAllService()
+        },
+        error: (data) => {
+          AppComponent.rzhAlt("error",data.info);
+          _this.qeuryAllService()
+        }
+      });
+    }else if(isNullOrUndefined(data.goodsPrice.batchPrice)){
+      AppComponent.rzhAlt("warning",'请先设置价格');
+      _this.qeuryAllService()
+    }
+  }
+
+  /**
+   * 批发价修改
+   */
+  submita(goodsCode,i) {
+      let url = '/goodsEdit/updateBatchPrice';
+      let data = {
+        goodsCode:goodsCode,
+        batchPrice:this._goods[i],
       }
-    });
+      console.log("█ data ►►►",  data.batchPrice);
+      this.submit.putRequest(url, data);
   }
 }
