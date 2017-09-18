bef9d50842ccef10480d93a383820b52a4d2a19b
gaojie <1226898330@qq.com>
2017-09-18 10:34:27 +0800

    up 中文日期控件

--- src/app/routes/orders/orders/orders.component.ts
+++ src/app/routes/orders/orders/orders.component.ts
@@ -1,23 +1,27 @@
 import {Component, OnInit} from "@angular/core";
 import {BsDatepickerConfig} from "ngx-bootstrap/datepicker";
+import {defineLocale} from "ngx-bootstrap/bs-moment";
+import {zhCn} from "ngx-bootstrap/locale";
+defineLocale('cn', zhCn);
+
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent implements OnInit {
-  public orderType:number = 1;
-  minDate: Date;
-  maxDate: Date;
-  locale = 'en';
+  public orderType: number = 1;
+  minDate: Date = new Date();
+  maxDate: Date = new Date();
   bsConfig: Partial<BsDatepickerConfig>;
 
   constructor() {
-    this.minDate = new Date();
-    this.maxDate = new Date();
-    this.minDate.setDate(this.minDate.getDate() - 1);
-    this.maxDate.setDate(this.maxDate.getDate() + 7);
-    this.bsConfig = Object.assign({}, {locale: this.locale});
+    this.bsConfig = Object.assign({}, {
+      locale: 'cn',
+      // minDate: this.minDate.getDate() - 1,
+      // maxDate: this.maxDate.getDate() + 7,
+      containerClass: 'theme-blue'
+    });
   }
 
   ngOnInit() {
