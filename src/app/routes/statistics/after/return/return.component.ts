6720315f9258859b41e2e485ac67acde564799ef
xiejiaojiao <2583206346@qq.com>
2017-09-21 12:01:59 +0800

    up 认证过的名称图标修改，并添加提示

--- src/app/routes/statistics/after/return/return.component.ts
+++ src/app/routes/statistics/after/return/return.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {BsDatepickerConfig} from "ngx-bootstrap/datepicker";
+import {zhCn} from "ngx-bootstrap/locale";
+import {defineLocale} from "ngx-bootstrap";
+import {FormBuilder, FormGroup} from "@angular/forms";
+defineLocale('cn', zhCn);
 
 @Component({
   selector: 'app-return',
@@ -6,8 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./return.component.scss']
 })
 export class ReturnComponent implements OnInit {
+  // private myForm: FormGroup;
+  // public orderType: number = 1;
+  // minDate: Date = new Date();
+  // maxDate: Date = new Date();
 
-  constructor() { }
+  datepickerModel: Date;
+  daterangepickerModel: Date[];
+  constructor(private formBuilder: FormBuilder) {
+
+    // this.myForm = this.formBuilder.group({}, {
+    //   locale: 'cn',
+    //   date: null,
+    //   range: null,
+    //   // minDate: this.minDate.getDate() - 1,
+    //   // maxDate: this.maxDate.getDate() + 7,
+    //   containerClass: 'theme-blue'
+    // });
+
+  }
   option = {
     color: ['#3398DB'],
     tooltip: {
