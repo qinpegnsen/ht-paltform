5b15151b4e540900a2061c4802992895fefd9a83
qps <744056574@qq.com>
2017-10-16 08:39:01 +0800

    up 代理商订单

--- src/app/routes/app/app.module.ts
+++ src/app/routes/app/app.module.ts
@@ -11,17 +11,24 @@ import { AppSetComponent } from './app-set/app-set.component';
 import {FileUploadModule} from 'ng2-file-upload';
 import {LogisticsComponent} from './logistics/logistics.component';
 import {AppSetService} from './app-set/app-set.service';
+import {SysNoticeComponent} from "./sys-notice/sys-notice.component";
+import {AddNoticeComponent} from "./sys-notice/add-notice/add-notice.component";
+import {OperationService} from "../operation/operation.service";
 
 
 // 子路由，用于页面嵌套显示
 const appChildRoutesfile: Routes = [
   {path: 'rightpage', component: RightpageComponent}
 ];
+const addNotice: Routes = [
+  {path: 'addNotice', component: AddNoticeComponent}
+];
 const routes: Routes = [
   {path: '', component: AppIndexOptComponent},
   {path: 'app-index-opt', component: AppIndexOptComponent,children: appChildRoutesfile},
   {path: 'app-index-tpl', component: AppIndexTplComponent,children: appChildRoutesfile},
-  {path: 'app-set', component: AppSetComponent}
+  {path: 'app-set', component: AppSetComponent},
+  {path: 'sys-notice', component: SysNoticeComponent,children: addNotice},
 ]
 
 @NgModule({
@@ -36,8 +43,9 @@ const routes: Routes = [
     AppIndexTplService,
     AppIndexOptComponent,
     AppIndexTplComponent,
-    AppSetService
+    AppSetService,
+    OperationService
   ],
-  declarations: [AppIndexOptComponent, AppIndexTplComponent, RightpageComponent, AppSetComponent,LogisticsComponent]
+  declarations: [AppIndexOptComponent, AddNoticeComponent,AppIndexTplComponent, RightpageComponent, AppSetComponent,LogisticsComponent,SysNoticeComponent]
 })
 export class AppModule { }
