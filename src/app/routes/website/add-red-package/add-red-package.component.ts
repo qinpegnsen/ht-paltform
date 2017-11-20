5c97cd8267c03e04202fceb732270ccce610e653
qps <744056574@qq.com>
2017-11-20 17:06:00 +0800

    up 红包新增

--- src/app/routes/website/add-red-package/add-red-package.component.ts
+++ src/app/routes/website/add-red-package/add-red-package.component.ts
@@ -1,20 +1,55 @@
 import {Component, OnInit} from '@angular/core';
 import {Location} from "@angular/common";
 import {WebstiteService} from "../webstite.service";
+import {BsDatepickerConfig} from "ngx-bootstrap/datepicker";
+import {listLocales} from "ngx-bootstrap/bs-moment";
+import {TimepickerConfig} from "ngx-bootstrap";
 const swal = require('sweetalert');
 declare var $: any;
+
+/**
+ * 对时间的配置
+ * @returns {TimepickerConfig&{hourStep: number, minuteStep: number, showMeridian: boolean, readonlyInput: boolean, mousewheel: boolean}}
+ */
+export function getTimepickerConfig(): TimepickerConfig {
+  return Object.assign(new TimepickerConfig(), {
+    hourStep: 1,
+    minuteStep: 1,
+    showMeridian: false,
+    readonlyInput: false,
+    mousewheel: true
+  });
+}
 
 @Component({
   selector: 'app-add-red-package',
   templateUrl: './add-red-package.component.html',
-  styleUrls: ['./add-red-package.component.scss']
+  styleUrls: ['./add-red-package.component.scss'],
+  providers: [{ provide: TimepickerConfig, useFactory: getTimepickerConfig }]//注入配置好的默认的时间
 })
 export class AddRedPackageComponent implements OnInit {
 
-  private moduleList = [];            //新增的红包规则的数组
-  private deletebutton;               //删除的按钮
-
+  public moduleList = [];             //新增的红包规则的数组
+  public deletebutton;                //删除的按钮
+  public datepickerModel: Date = new Date();  //红包的日期
+  public minDate: Date = new Date();          //默认最小的日期
+  // public minTime: Date = new Date();          //默认显示的最小的时分秒
+  public showSec: boolean = true;           //是否显示秒，默认不显示
+  public myTime: string;                    //红包的时分秒
+  public totalNum: string;                  //红包的总数
+  public totalPrice: string;                //红包的总额
+  bsConfig: Partial<BsDatepickerConfig>;
+  locale: 'cn';
+  locales = listLocales();
   constructor(public location: Location,public service: WebstiteService) {
+    // this.minTime.setHours(8);//最小的时间
+    // this.minTime.setMinutes(0);
+    this.bsConfig = Object.assign({}, {
+      locale: this.locale,
+      dateInputFormat: 'YYYY-MM-DD',//将时间格式转化成年月日的格式
+      containerClass: 'theme-blue',
+      rangeInputFormat: 'YYYY-MM-DD'
+    });
   }
 
   ngOnInit() {
