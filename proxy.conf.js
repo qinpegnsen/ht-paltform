0ff405397b98efe10a781bcdcd8cd81a55de57e5
qps <744056574@qq.com>
2017-11-29 11:34:45 +0800

    up 红包规则的引导语

--- proxy.conf.js
+++ proxy.conf.js
@@ -62,7 +62,7 @@ const PROXY_CONFIG = [
       "/rpCustAcctRec",
       "/rpAccount"
     ],
-    target: zyg  + "8084",   //拦截 context配置路径，经过此地址
+    target: gh  + "8084",   //拦截 context配置路径，经过此地址
     secure: false
   },
   {
@@ -71,7 +71,7 @@ const PROXY_CONFIG = [
       "/upload",
       "/res"
     ],
-    target: zyg    + "8082",   //拦截 context配置路径，经过此地址
+    target: gh  + "8082",   //拦截 context配置路径，经过此地址
     secure: false
   }
 ];
