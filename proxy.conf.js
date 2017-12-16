d62ef759e3e1a4f12af149fdf7b67c1e360c9605
gaojie <1226898330@qq.com>
2017-12-16 19:23:46 +0800

    up 路由代理配置

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,11 +1,12 @@
-  const bb = 'http://192.168.10.178:';  //伯白
-  const br = 'http://192.168.10.110:';  //伯融
-  const sz = 'http://192.168.10.112:';  //尚泽
-  const sg = 'http://192.168.10.111:';  //善谷
-  const zyg = 'http://192.168.10.167:'; //张阳光
-  const gh = 'http://192.168.10.109:';  //高辉
-  const wp = 'http://192.168.10.182:';  //万鹏
-  const csj = 'http://192.168.10.221:';  //测试机
+const bb = 'http://192.168.10.178:';  //伯白
+const br = 'http://192.168.10.110:';  //伯融
+const sz = 'http://192.168.10.112:';  //尚泽
+const sg = 'http://192.168.10.111:';  //善谷
+const zyg = 'http://192.168.10.167:'; //张阳光
+const gh = 'http://192.168.10.109:';  //高辉
+const wp = 'http://192.168.10.182:';  //万鹏
+const csj = 'http://192.168.10.221:';  //测试机
+const ly = 'http://192.168.10.101:';  //柳阳
 
 const PROXY_CONFIG = [
   {
@@ -61,9 +62,12 @@ const PROXY_CONFIG = [
       "/rpSetting",
       "/rpCustAcctRec",
       "/rpAccount",
-      "/rpStore"
+      "/rpStore",
+      "/rpCustWithdraw",
+      "/enterprise",//企业入驻
+      "/store"//店铺
     ],
-    target: gh  + "8084",   //拦截 context配置路径，经过此地址
+    target: ly + "8084",   //拦截 context配置路径，经过此地址
     secure: false
   },
   {
@@ -72,14 +76,7 @@ const PROXY_CONFIG = [
       "/upload",
       "/res"
     ],
-    target: gh   + "8082",   //拦截 context配置路径，经过此地址
-    secure: false
-  },
-  {
-    context: [
-      "/rpCustWithdraw"
-    ],
-    target: gh  + "8084",   //拦截 context配置路径，经过此地址
+    target: csj + "8082",   //拦截 context配置路径，经过此地址
     secure: false
   }
 ];
