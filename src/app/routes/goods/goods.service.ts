4ff940bc4b0b6ebeb5171980beec0f01cd13e8cd
gaojie <1226898330@qq.com>
2017-12-27 04:24:00 +0800

    up 修改商品发布成功跳转路径

--- src/app/routes/goods/goods.service.ts
+++ src/app/routes/goods/goods.service.ts
@@ -91,7 +91,7 @@ export class GoodsService {
             AppComponent.rzhAlt("success", '操作成功');
             me.location.back();
           } else {
-            me.router.navigate(['/main/goods/publish/step_three'], {queryParams: {baseCode: res.data}})
+            me.router.navigate(['/main/goods/plat/publish/step_three'], {queryParams: {baseCode: res.data}})
           }
         } else {
           MaskService.hideMask();//当上传图片之后才提交数据的话，遮罩层开启是在图片上传之前，所以需要手动关闭
