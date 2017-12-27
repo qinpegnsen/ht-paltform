4344e2ceb0c4c8213bd8b33e2fc1b63792489846
qps <744056574@qq.com>
2017-12-27 10:07:21 +0800

    up 删除之前多余的代码

--- src/app/routes/sale/sale.module.ts
+++ src/app/routes/sale/sale.module.ts
@@ -1,16 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RefundControlComponent} from './refund-control/refund-control.component';
-import {RefundVerifyComponent} from './refund-verify/refund-verify.component';
 import {ReturnControlComponent} from './return-control/return-control.component';
-import {ReturnVerifyComponent} from './return-verify/return-verify.component';
 import {MsgComponent} from './msg/msg.component';
 import {RouterModule, Routes} from "@angular/router";
 import {SharedModule} from "../../shared/shared.module";
 import {AfterDetailsComponent} from './after-details/after-details.component';
 import {AddproblemComponent} from './addproblem/addproblem.component';
 import {AfterService} from "./after.service";
-import {InspectGoodsComponent} from './inspect-goods/inspect-goods.component';
 import {OrderDetailComponent} from "./order-detail/order-detail.component";
 import {OrdersService} from "../orders/orders/orders.service";
 import {StoreRefundComponent} from "./store-refund/store-refund.component";
@@ -21,7 +18,6 @@ const problemcomplem: Routes = [
 const routes: Routes = [
   {path: '', redirectTo: 'refund-control'},
   {path: 'enterprise', children: [
-    {path: '',redirectTo: 'storeRefundMan'},
     {path: 'storeRefundMan', component: StoreRefundComponent, children: [
       {path: 'after-details', component: AfterDetailsComponent},
       {path: 'order-details', component: OrderDetailComponent}
@@ -35,24 +31,11 @@ const routes: Routes = [
     {path: 'after-details', component: AfterDetailsComponent},
     {path: 'order-details', component: OrderDetailComponent}
   ]},
-  // {path: 'refund-verify', component: RefundVerifyComponent, children: [
-  //   {path: 'after-details', component: AfterDetailsComponent},
-  //   {path: 'order-details', component: OrderDetailComponent}
-  // ]},
   {path: 'return-control', component: ReturnControlComponent, children: [
     {path: 'after-details', component: AfterDetailsComponent},
     {path: 'order-details', component: OrderDetailComponent}
   ]},
-  // {path: 'return-verify', component: ReturnVerifyComponent, children: [
-  //   {path: 'after-details', component: AfterDetailsComponent},
-  //   {path: 'order-details', component: OrderDetailComponent}
-  // ]},
-  // {path: 'inspect-goods', component: InspectGoodsComponent, children: [
-  //   {path: 'after-details', component: AfterDetailsComponent},
-  //   {path: 'order-details', component: OrderDetailComponent}
-  // ]},
   {path: 'msg', component: MsgComponent, children: problemcomplem},
-
 ];
 @NgModule({
   imports: [
@@ -62,21 +45,16 @@ const routes: Routes = [
   ],
   declarations: [
     RefundControlComponent,
-    RefundVerifyComponent,
     ReturnControlComponent,
-    ReturnVerifyComponent,
     MsgComponent,
     AfterDetailsComponent,
     OrderDetailComponent,
     AddproblemComponent,
-    InspectGoodsComponent,
     StoreRefundComponent,
     StoreReturnComponent],
   providers: [
     RefundControlComponent,
-    RefundVerifyComponent,
     ReturnControlComponent,
-    ReturnVerifyComponent,
     MsgComponent,
     AddproblemComponent,
     AfterService,
