6b91c1dce948a6c26ebed4a2cc1f319a195384a7
qps <744056574@qq.com>
2018-01-08 17:16:26 +0800

    up 红包提现的排序

--- src/app/routes/activities/audit/audit.component.ts
+++ src/app/routes/activities/audit/audit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PageEvent} from "../../../shared/directives/ng2-datatable/DataTable";
 import {isNullOrUndefined, isUndefined} from "util";
 import {Page} from "../../../core/page/page";
@@ -11,21 +11,22 @@ import {ActivitiesService} from "../activities.service";
 })
 export class AuditComponent implements OnInit {
 
-  public auditListData:any;                           //审核列表的数据
-  public curState:string='';                           //当前的状态
-  public dealButton:any;                               //处理申请的按钮
+  public auditListData: any;                           //审核列表的数据
+  public curState: string = '';                           //当前的状态
+  public dealButton: any;                               //处理申请的按钮
   public showStroeInvest: boolean = false;            //企业投资的弹窗
   public currentId: any;                               //当前的id
   public amountData: any;                              //获取提现总额和未提现总额
-  public showList: boolean=true;                      //是否显示组件
+  public showList: boolean = true;                      //是否显示组件
 
-  constructor(public activitiesService:ActivitiesService) { }
+  constructor(public activitiesService: ActivitiesService) {
+  }
 
   ngOnInit() {
-    this.qeuryAll('CR',1);
+    this.qeuryAll('CR', 1);
     this.getWithDrawTotal();
-    this.dealButton={
-      title:"处理",
+    this.dealButton = {
+      title: "处理",
       type: "update"
     };
   }
@@ -34,12 +35,12 @@ export class AuditComponent implements OnInit {
   /**
    * 获取提现总额和未提现总额
    */
-  getWithDrawTotal(){
+  getWithDrawTotal() {
     let url = "/rpCustWithdraw/sumWithDraw";
-    let data={};
-    let result=this.activitiesService.queryWithDrawAmount(url,data);
-    if(result){
-      this.amountData=result;
+    let data = {};
+    let result = this.activitiesService.queryWithDrawAmount(url, data);
+    if (result) {
+      this.amountData = result;
     }
   }
 
@@ -56,30 +57,30 @@ export class AuditComponent implements OnInit {
    */
   getAddRecordData(data) {
     this.currentId = null;
-    if(data.type) this.qeuryAll(this.curState,data.page)
+    if (data.type) this.qeuryAll(this.curState, data.page)
   }
 
   /**
    * 审核默认通过
    */
-  auditPass(id){
+  auditPass(id) {
     let url = "/rpCustWithdraw/updateStateTODeal";
-    let data={
-      id:id
+    let data = {
+      id: id
     };
-    this.activitiesService.updateStateTODeal(url,data);
-    this.qeuryAll(this.curState,1)
+    this.activitiesService.updateStateTODeal(url, data);
+    this.qeuryAll(this.curState, 1)
   }
 
   /**
    * 提现申请的列表
    */
-  qeuryAll(state,curPage,event?: PageEvent){
-    if(state){
-      this.curState=state;
-    }else{
-      if(!this.curState){
-        this.curState='CR';
+  qeuryAll(state, curPage, event?: PageEvent) {
+    if (state) {
+      this.curState = state;
+    } else {
+      if (!this.curState) {
+        this.curState = 'CR';
       }
     }//解决分页切换标签后分页刷新的问题
     let me = this, activePage = 1;
@@ -87,14 +88,24 @@ export class AuditComponent implements OnInit {
       activePage = event.activePage;
     } else if (!isUndefined(curPage)) {
       activePage = curPage;
-    };
+    }
+    ;
+    let sortColumns;//定义排序规则
+    if (this.curState == 'CR') {
+      sortColumns='create_time DESC'
+    }else if (this.curState == 'DEAL') {
+      sortColumns='deal_time DESC'
+    }else{
+      sortColumns='done_time DESC'
+    }
     let url = "/rpCustWithdraw/query";
-    let data={
+    let data = {
       curPage: activePage,
-      pageSize:10,
-      state:this.curState,
+      pageSize: 10,
+      state: this.curState,
+      sortColumns: sortColumns
     };
-    let result = this.activitiesService.queryWithDrawData(url,data);
+    let result = this.activitiesService.queryWithDrawData(url, data);
     me.auditListData = new Page(result);
   };
 
